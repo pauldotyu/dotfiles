8c090219f2b69d6f63c7583d708de5560081da52
Paul Yu <paul.d.yu@gmail.com>
2023-02-15 19:47:45 -0800

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-alias k=kubectl
-complete -o default -F __start_kubectl k
-source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
-echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
+chmod 700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp -p "$PWD/dotfiles/.zshrc" "$HOME"
