e1f0af77f792b44ac812b8e091ec52d8cc1e82ab
Paul Yu <paul.d.yu@gmail.com>
2023-02-15 19:32:47 -0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
+source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
+echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
